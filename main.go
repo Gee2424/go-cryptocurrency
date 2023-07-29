ef4c1197680613a80057881252e8d21b69ed9627
Gee2424 <88376078+Gee2424@users.noreply.github.com>
2023-07-29 23:13:34 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	// Initialize a new blockchain
+	bc := NewBlockchain()
+
+	// Initialize a new wallet
+	w := NewWallet()
+
+	// Use command line arguments to determine behavior
+	addTransaction := flag.Bool("add", false, "Add a transaction")
+	amount := flag.Int("amount", 0, "Amount of the transaction")
+	receiver := flag.String("receiver", "", "Receiver of the transaction")
+	flag.Parse()
+
+	if *addTransaction {
+		if *amount == 0 || *receiver == "" {
+			fmt.Println("You must include an amount and receiver with the -amount and -receiver flags.")
+			return
+		}
+
+		// Create and sign a transaction
+		t := w.createTransaction(*receiver, *amount)
+
+		// Add the transaction to a block
+		bc.AddBlock(t)
+	} else {
+		// Print out the blockchain
+		for _, block := range bc.blocks {
+			fmt.Printf("Previous hash: %s\n", block.PrevBlockHash)
+			fmt.Printf("Transaction: %s\n", block.Transaction)
+			fmt.Printf("Hash: %s\n", block.Hash)
+			fmt.Println()
+		}
+	}
 }
